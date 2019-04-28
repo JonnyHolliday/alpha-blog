8ec5e9b2be8a8e36179420b8543de276ac049bab
Jonny H repository <jonny@the-hollidays.co.uk>
2019-04-28 16:01:08 +0100

    Completed New process with error messages and confirm screen

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,7 +7,17 @@ class ArticlesController < ApplicationController
   def create
   	# render plain: params[:article].inspect
   	@article = Article.new(article_params)
-  	@article.save
+  	if @article.save
+  	  flash[:notice] = "Article was successfully created"
+  	  redirect_to article_path(@article) #gives it somewhere to go after the insert
+  	else
+  	  render 'new' # re-dislay the new article form
+  	end
+  	  	
+  end
+
+  def show
+  	@article = Article.find(params[:id])
   end
 
   private
