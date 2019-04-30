7531281667127fe4088e311d33afc5a9de6c7247
Jonny H repository <jonny@the-hollidays.co.uk>
2019-04-30 20:42:42 +0100

    Added form actions for create and update with show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,16 @@
 class ArticlesController < ApplicationController
 
+  def index
+  	@articles = Article.all
+  	
+  end
+
   def new
   	@article  = Article.new
+  end
+
+  def edit
+  	@article = Article.find(params[:id])
   end
 
   def create
@@ -14,6 +23,17 @@ class ArticlesController < ApplicationController
   	  render 'new' # re-dislay the new article form
   	end
   	  	
+  end
+
+  def update
+  	@article = Article.find(params[:id])
+  	if @article.update(article_params)
+  	  flash[:notice] = "Article was succesfully updated"
+  	  redirect_to article_path(@article)
+  	else
+  	  render 'edit'
+  	end
+
   end
 
   def show
