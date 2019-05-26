1e00f8d1aac8854ae03b1b3769a30ab678a81888
Jonny H repository <jonny@the-hollidays.co.uk>
2019-05-26 18:29:27 +0100

    Login fails so adding devise_scope in routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ resources :users, except: [:new]
 
 get 'login', to: 'sessions#new'
 post 'login', to: 'sessions#create'
-delete 'logout', to: 'sessions#destroy'
+devise_scope :user do
+  delete 'logout', to: 'devise/sessions#destroy'
+end
+#delete 'logout', to: 'sessions#destroy'
 
 end
